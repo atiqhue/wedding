6b7716af1dece776929a366b56e66bbbb921de74
Atiqah <145252222+atiqhue@users.noreply.github.com>
2025-10-22 09:04:10 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+body {
+  font-family: "Poppins", sans-serif;
+  text-align: center;
+  background: #fff7f8;
+  color: #444;
+  margin: 0;
+}
+
+.hero {
+  background: linear-gradient(#ffeef0, #fff7f8);
+  padding: 100px 20px;
+}
+
+.hero h1 {
+  font-size: 3em;
+  color: #e58ca6;
+}
+
+button {
+  background: #e5a5b6;
+  border: none;
+  padding: 10px 25px;
+  color: white;
+  border-radius: 25px;
+  cursor: pointer;
+}
